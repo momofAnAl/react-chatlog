bef785a0a87623c1fa1c3ed2b200cfbae62118b7
anhtran <anhtr077@gmail.com>
2024-12-11 22:01:54 -0800

    Update chatlog to render all entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
 import './components/ChatEntry';
 import ChatEntry from './components/ChatEntry';
-import ChatLog from './components/CharLog'
+import ChatLog from './components/CharLog';
 import './components/TimeStamp';
+import messages from './data/messages.json';
 
 const App = () => {
   return (
@@ -13,9 +14,8 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
-        <ChatEntry>
-          
-        </ChatEntry>
+        <ChatLog entries={messages} />
+
       </main>
     </div>
   );
